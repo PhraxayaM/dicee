aeee67040d1450b96dabb1577f1511c4bee44128
MattHew Phraxayavong <mphraxa@gmail.com>
2019-07-16 08:12:36 -0700

    UI elements implemented through storyboard

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by MattHew Phraxayavong on 7/11/19.
+//  Copyright © 2019 MattHew Phraxayavong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+
+}
+
